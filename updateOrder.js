af08a6eb82c2f3fc006f8b154e1506e241257580
Jmcus <vluxyue@gmail.com>
2014-11-01 22:12:18 +0800

    cookie

--- updateOrder.js
+++ updateOrder.js
@@ -2,10 +2,10 @@ function updateOrder(){
     const TAX = 0.0925;
     const DONUTPRICE = 0.50;
 
-    var numCake = parseInt(doucument.getElemetById("cakedonuts").value);
-    var numGlazed = parseInt(doucument.getElemetById("glazeddonuts").value);
+    var numCake = parseInt(document.getElemetById("cakedonuts").value);
+    var numGlazed = parseInt(document.getElemetById("glazeddonuts").value);
 
-    if(isNaN(numCake))    //如果返回true，表示数据不是数字
+    if(isNaN(numCake))    //如果返回true，表示数据不是数字（PS：检查例外情况比较合理）
         numCake = 0;
     if(isNaN(numGlazed))
         numGlazed = 0;
@@ -13,8 +13,8 @@ function updateOrder(){
     var subTotal = (numCake + numGlazed) * DONUTPRICE;
     var tax = subTotal +TAX;
     var total = subTotal + tax;
-    doucument.getElemetById("subtotal").value = "$" + subTotal.toFixed(2);    //四舍五入到小数点后两位
-    doucument.getElemetById("tax").value = "$" + tax.toFixed(2);
-    doucument.getElemetById("total").value = "$" + total.toFixed(2);
+    document.getElemetById("subtotal").value = "$" + subTotal.toFixed(2);    //四舍五入到小数点后两位
+    document.getElemetById("tax").value = "$" + tax.toFixed(2);
+    document.getElemetById("total").value = "$" + total.toFixed(2);
 
 }
